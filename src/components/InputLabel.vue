f142def5ffdf286073d3970e03a756c10fcc9a7a
Fahim Anzam Dip <dfahimanzam@gmail.com>
2023-08-29 18:34:26 +0600

    Design Fix

--- src/components/InputLabel.vue
+++ src/components/InputLabel.vue
@@ -3,5 +3,5 @@ defineProps(['title', 'required'])
 </script>
 
 <template>
-  <label class="font-medium" :class="required ? 'after:content-[\'*\'] after:ml-0.5 after:text-red-500' : ''">{{ title }}</label>
+  <label class="font-medium after:content-['*'] after:ml-0.5" :class="required === true ? 'after:text-red-500' : 'after:text-transparent'">{{ title }}</label>
 </template>
--- src/sections/Works.vue
+++ src/sections/Works.vue
@@ -89,12 +89,23 @@ let showAllWork = ref(false)
           </Project>
         </div>
 
-        <div :class="showAllWork ? 'hidden' : 'block'" class="absolute z-20 inset-x-0 bottom-0 flex justify-center bg-gradient-to-t pt-72 pb-8 from-[#0B1120]">
+        <div v-if="!showAllWork" class="absolute z-20 inset-x-0 bottom-0 flex justify-center bg-gradient-to-t pt-72 pb-8 from-[#0B1120]">
           <NButton @click="showAllWork = !showAllWork" btn-type="filled" class="px-4 py-2 text-lg font-normal tracking-wide" type="button">
             <span>See More</span>
             <template #icon>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m-6 3.75l3 3m0 0l3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />
+              </svg>
+            </template>
+          </NButton>
+        </div>
+
+        <div v-else class="flex justify-center mt-20 group">
+          <NButton @click="showAllWork = !showAllWork" btn-type="filled" class="px-4 py-2 text-lg font-normal tracking-wide" type="button">
+            <span>See Less</span>
+            <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />
               </svg>
             </template>
           </NButton>
