ef3cb38346d4c214a672840eda0e9447f067bb88
Fahim Anzam Dip <dfahimanzam@gmail.com>
2023-08-30 20:09:14 +0600

    Changed Vite Based Path

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -10,7 +10,7 @@ const year = new Date().getFullYear()
   <!-- Footer -->
   <footer :class="loadMobileNav ? 'pb-24 pt-6' : 'py-6'" class="bg-[#0B1120] text-slate-300">
     <p class="text-center text-sm md:text-base">
-      {{ year }} &copy; Designed & Built by <a target="_blank" href="https://github.com/FahimAnzamDip" class="text-secondary-500 font-medium hover:underline hover:underline-offset-4">Fahim Anzam Dip</a>
+      {{ year }} &copy; Designed & Built by <a target="_blank" href="https://github.com/FahimAnzamDip" class="text-secondary-400 font-medium underline underline-offset-4">Fahim Anzam Dip</a>
     </p>
   </footer>
   <!-- Footer -->
